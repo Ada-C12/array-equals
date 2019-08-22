db35daff14a8c50d44e12bc53d83fdbbf9728124
Sabrina Lowney <sabrina@galaxylaughing.com>
2019-08-22 12:28:53 -0700

    refactored array_equals argument checking

--- lib/array_equals.rb
+++ lib/array_equals.rb
@@ -1,19 +1,10 @@
 # Determines if the two input arrays have the same count of elements
 # and the same integer values in the same exact order
 def array_equals(array1, array2)
-  # raise NotImplementedError
   
   if array1 == nil && array2 == nil
     return true
-  elsif array1 == nil && !(array2 == nil)
-    return false
-  elsif !(array1 == nil) && array2 == nil
-    return false
-  elsif array1.length == 0 && array2.length == 0
-    return true
-  elsif array1.length == 0 && array2.length > 0
-    return false
-  elsif array1.length > 0 && array2.length == 0
+  elsif array1 == nil || array2 == nil
     return false
   elsif !(array1.length == array2.length)
     return false
