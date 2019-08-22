a1ebfc95158e0f6462aa355c994443a058b7a01e
Natalie Tapias <nataliemtapias@gmail.com>
2019-08-22 12:28:15 -0700

    Have thought about a way to determine the length of two parameters array1 and array2. Need to test and refine this method.

--- lib/array_equals.rb
+++ lib/array_equals.rb
@@ -1,5 +1,26 @@
 # Determines if the two input arrays have the same count of elements
 # and the same integer values in the same exact order
 def array_equals(array1, array2)
+  
+  array1_length = 0
+  until array1[i] == nil
+    array1_length += 1
+  end
+  
+  if array1_length == 0
+    # this means that this is an empty array
+  end
+  
+  array2_length = 0
+  until array[i] == nil
+    array2_length += 1
+  end
+  
+  
+  
+  # if x
+  # return true
+  # else
+  # return false
   raise NotImplementedError
 end
