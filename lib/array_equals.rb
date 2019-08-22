48e40890409cabb582963a985f68d40cdb414aa1
geli-gel <angele.zam@gmail.com>
2019-08-22 13:44:02 -0700

    added body of array_equals method

--- lib/array_equals.rb
+++ lib/array_equals.rb
@@ -1,5 +1,24 @@
 # Determines if the two input arrays have the same count of elements
 # and the same integer values in the same exact order
+# you can use .each or .each_with_index but no other array class methods
+# you can use .length and comparisons
 def array_equals(array1, array2)
-  raise NotImplementedError
+  answer = true
+  if array1 == nil && array2 == nil
+    return answer
+  elsif array1 == nil && array2 != nil || \
+     array2 == nil && array1 != nil || \
+     array1.length != array2.length
+
+    answer = false
+  else
+    array1.each_with_index do |elem, index|
+      if answer == false
+        return answer
+      else
+      elem == array2[index] ? answer = true : answer = false
+      end
+    end
+  end
+  return answer
 end
