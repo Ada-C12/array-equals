8a951ae6168a3a4aefb09a36bbc1801b1deefbd7
Emily Vomacka <emilyvomacka@gmail.com>
2019-08-22 13:29:34 -0700

    added array method which passes all tests

--- lib/array_equals.rb
+++ lib/array_equals.rb
@@ -1,5 +1,15 @@
 # Determines if the two input arrays have the same count of elements
 # and the same integer values in the same exact order
 def array_equals(array1, array2)
-  raise NotImplementedError
-end
+  if !array1 && !array2
+    return true
+  elsif array1 && array2 && array1.length == array2.length
+    array1.each_with_index { |element, index| 
+      if element != array2[index]
+        return false
+      end 
+    }
+    return true
+  end
+  return false 
+end 
