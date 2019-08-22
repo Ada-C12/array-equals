461eb89ec1aa7d3feba1ab9f449a0667bf98b49d
daniela sanchez <dnsanche@gmail.com>
2019-08-22 15:03:29 -0700

    Only tests for nil arrays aren't passing

--- lib/array_equals.rb
+++ lib/array_equals.rb
@@ -1,5 +1,20 @@
 # Determines if the two input arrays have the same count of elements
 # and the same integer values in the same exact order
 def array_equals(array1, array2)
+true_false_array = []
+  if array1.length == array2.length
+    array1.length.times do |index|
+      if array1[index] == array2[index]
+        true_false_array << "true" 
+      else 
+        true_false_array  << "false" 
+      end
+    end
+  else 
+    true_false_array  << "false" 
+  end
+
+  return true_false_array.delete("false") == nil
   raise NotImplementedError
 end
+
