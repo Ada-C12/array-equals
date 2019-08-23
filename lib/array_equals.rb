3eb166cd87a21a2f570d692bf404b63e765bdf6e
Raisah Vesteinsdottir <rvesteinsdottir@gmail.com>
2019-08-23 15:32:32 -0700

    Refactored array_equals method

--- lib/array_equals.rb
+++ lib/array_equals.rb
@@ -3,30 +3,14 @@
 # Array Equals Exercise
 
 def array_equals(array1, array2)
-  if array1 == nil || array2 == nil
-    if array1 == array2
-      return true
-    else
-      return false
-    end
-  end
+  return  array1 == array2 if array1 == nil || array2 == nil
   
-  if array1.length == 0 || array2.length == 0
-    if array1.length == array2.length
-      return true
-    else
-      return false
-    end
-  end
+  return array1.length == array2.length if array1.length == 0 || array2.length == 0
   
-  if array1.length != array2.length
-    return false
-  end
+  return false if array1.length != array2.length
   
   array1.each_with_index do |value, index|
-    if value != array2[index]
-      return false
-    end
+    return false if value != array2[index]
   end
   
   return true
