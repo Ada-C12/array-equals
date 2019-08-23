ce799d67dcf8abdcc0c4821122aa016b77788bc4
Janice Huang <janicehuang@gmail.com>
2019-08-22 21:46:12 -0700

    clean up further

--- lib/array_equals.rb
+++ lib/array_equals.rb
@@ -2,9 +2,7 @@ def array_equals(array1, array2)
   array1 = [] if array1 == nil
   array2 = [] if array2 == nil
 
-  if array1.length != array2.length
-    return false
-  end
+  return false if array1.length != array2.length
 
   index = 0
   while index < array1.length
