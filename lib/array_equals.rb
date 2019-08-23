b447c935e4aaadaa9d19601e1f2ed82ede700c62
Dianna Tingg <dtingg@gmail.com>
2019-08-23 14:42:21 -0700

    fixed bug for last element and created new test

--- lib/array_equals.rb
+++ lib/array_equals.rb
@@ -1,35 +1,30 @@
 # Determines if the two input arrays have the same count of elements
 # and the same integer values in the same exact order
+
 def array_equals(array1, array2)
-  
-  if !array1 && !array2
-    return true
-  elsif !array1 && array2
-    return false
-  elsif array1 && !array2
-    return false  
-  end
-  
-  if array1 == [] && array2 == []
-    return true
-  elsif array1 == [] && array2 != []
-    return false
-  elsif array1 != [] && array2 == []
+  # Check if either array is nil
+  if !array1 || !array2
+    return true if !array1 && !array2
     return false
   end
   
+  # Check if either array is empty
+  if array1 == [] || array2 == []
+    return true if array1 == [] && array2 == []
+    return false
+  end
+  
+  # Check for mismatch in array length
   if array1.length != array2.length
     return false
   end
   
   length = array1.length
   
-  (length - 1).times do |i|
+  length.times do |i|
     if array1[i] != array2[i]
-      return false
-      
+      return false    
     end
   end
-  
   return true
 end
