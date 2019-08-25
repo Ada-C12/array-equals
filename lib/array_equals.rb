7e5e9f27fe75edea1d1e75c68f21c42208446464
Eve Le <nguyenthao.lee@gmail.com>
2019-08-24 22:20:59 -0700

    Refactored array_equals method

--- lib/array_equals.rb
+++ lib/array_equals.rb
@@ -6,16 +6,17 @@ def array_equals(array1, array2)
   elsif array1 && array2
     size1 = array1.length
     size2 = array2.length
-    if size1 != size2
-      return false
-    end
-    
-    size1.times do |index|
-      if array1[index] != array2[index]
-        return false
+    # if both arrays are not nil and equal in size, compare their elements
+    if size1 == size2    
+      size1.times do |index|
+        # return false early if any 2 elements at same index found not equal
+        if array1[index] != array2[index]
+          return false
+        end
       end
+      return true
     end
-    return true
   end
+
   return false
 end
