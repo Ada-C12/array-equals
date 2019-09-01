0364f9f6f34bb4349eddab081cf2fb2ec81dc8ef
Amal Hassan-Ali <amalh.a97@gmail.com>
2019-08-31 17:46:30 -0700

    removed variable

--- lib/array_equals.rb
+++ lib/array_equals.rb
@@ -1,7 +1,6 @@
 # Determines if the two input arrays have the same count of elements
 # and the same integer values in the same exact order
 
-require 'pry'
 def array_equals(array1, array2)
   
   
@@ -14,18 +13,13 @@ def array_equals(array1, array2)
   end 
   
   
-  if array1.length > array2.length 
-    big_arr = array1 
-    small_arr = array2 
-    
-  else
-    big_arr = array2 
-    small_arr = array1 
-    
+  if array1.length != array2.length 
+    return false 
   end 
-  
-  big_arr.each_with_index do |num, index|
-    if num != small_arr[index]
+
+   
+  array1.each_with_index do |num, index|
+    if num != array2[index]
       return false 
     end 
     
