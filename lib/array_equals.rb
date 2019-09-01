8f554fe9ec34732baabc22230c35c48529b05f6f
Natalie Tapias <nataliemtapias@gmail.com>
2019-09-01 08:12:25 -0700

    Made use of each_with_index enumerable to compare two different arrays.

--- lib/array_equals.rb
+++ lib/array_equals.rb
@@ -1,45 +1,22 @@
 
 def array_equals(first_array, second_array)
   
-  if first_array.nil? && second_array.nil? 
-    return true
-  elsif first_array.nil? || second_array.nil?
+  if (first_array.nil? && !second_array.nil?) || (second_array.nil? && !first_array.nil?)
     return false
-  elsif first_array.length != second_array.length
+  elsif second_array.nil? && first_array.nil?
+    return true
+  end
+  
+  if first_array.length != second_array.length
     return false
   end
   
   flag = 0
-  first_array.each.each_with_index do |index, value|
+  first_array.each.each_with_index do |value, index|
     if value != second_array[index]
       flag += 1
     end
   end
   
-  flag > 0 ? true : false
-  
-  
+  flag > 0 ? false : true
 end
-#   if (first_array == nil && second_array != nil) || (second_array == nil && first_array != nil)
-#     return false
-#   elsif second_array == nil && first_array == nil
-#     return true
-#   end
-
-
-
-
-#   incrementer = 0
-#   first_array.each_with_index do |integer, i|
-#     if integer == second_array[i]
-#       incrementer += 1
-#     end
-#   end
-
-#   if incrementer == second_array.length && incrementer == first_array.length
-#     return true
-#   else
-#     return false
-#   end
-# end
-
