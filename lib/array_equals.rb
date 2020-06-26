d96ffbb13d57d6cc6279abc6ce5fc73ba1474ae7
Erika Maust <jamjar@Erikas-MacBook-Pro.local>
2020-06-26 11:20:58 -0700

    Redo of assignent

--- lib/array_equals.rb
+++ lib/array_equals.rb
@@ -1,28 +1,22 @@
 # Determines if the two input arrays have the same count of elements
 # and the same integer values in the same exact order
+
 def array_equals(array1, array2)
   
-  match = true
-  array1.each_with_index do |value, index|
-    if array1[index] != array2[index]
-      match = false
-    end
-    return match
-  end
-  
-  
-  if array1.length != array2.length
-    match == false
-    if array1 == nil && array2 != nil 
-      match false 
-    elsif array1 == [] && array2 == []
-      match false
-    end
-    return match
-  end
+  index = 0
+
+  return false if array1.nil? && !array2.nil?
+  return false if !array1.nil? && array2.nil?
+
+  until array1[index].nil? && array2[index].nil?
+
+      return false if array1[index].nil? && !array2[index].nil? || !array1[index].nil? && array2[index].nil?
+
+      return false if array1[index] != array2[index]
+
+      index += 1
 end
-array1 = [3,5]
-array2 = [4,3,5]
-puts array_equals(array1, array2)
 
+  return true
 
+end
